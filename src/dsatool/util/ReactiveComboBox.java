d1ee0de7723d4841124081349fadb45b27c74d36
errt <dominik.helm@stud.tu-darmstadt.de>
2017-12-13 19:08:15 +0100

    Vorschläge auch für editierbare Comboboxen

--- src/dsatool/util/ReactiveComboBox.java
+++ src/dsatool/util/ReactiveComboBox.java
@@ -15,10 +15,12 @@
  */
 package dsatool.util;
 
+import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.util.StringConverter;
@@ -28,37 +30,50 @@ public class ReactiveComboBox<T> extends ComboBox<T> {
 	private long lastChange = 0;
 
 	private final ChangeListener<String> textListener = (observable, oldValue, newValue) -> {
-		final String text = getEditor().getText();
+		final TextField editor = getEditor();
+		final int previousCaret = editor.getCaretPosition() + 1;
+		final String text = newValue;
 
-		final StringConverter<T> converter = getConverter();
-		if (converter != null) {
-			final T value = converter.fromString(text);
-			setValue(value);
+		T item = null;
+		if (previousCaret > 0 && previousCaret <= text.length()) {
+			item = findItem(editor.getText().substring(0, previousCaret));
+		}
+
+		if (item != null) {
+			editor.setText(item.toString());
+			Platform.runLater(() -> {
+				editor.positionCaret(editor.getText().length());
+				editor.selectPositionCaret(previousCaret);
+			});
+		} else {
+			final StringConverter<T> converter = getConverter();
+			if (converter != null) {
+				setValue(converter.fromString(text));
+			}
 		}
 	};
 
 	private final EventHandler<KeyEvent> searchHandler = event -> {
-		final long current = System.currentTimeMillis();
+		if (!isEditable()) {
+			final long current = System.currentTimeMillis();
 
-		if (current > lastChange + 750) {
-			searchString.setLength(0);
-		}
-		lastChange = current;
-		final KeyCode code = event.getCode();
-		if (code == KeyCode.BACK_SPACE && searchString.length() > 0) {
-			searchString.setLength(searchString.length() - 1);
-		} else if (code == KeyCode.ESCAPE) {
-			searchString.setLength(0);
-		} else if (!code.isArrowKey() && !code.isFunctionKey() && !code.isMediaKey() && !code.isModifierKey()) {
-			searchString.append(event.getText());
-		} else
-			return;
+			if (current > lastChange + 750) {
+				searchString.setLength(0);
+			}
+			lastChange = current;
+			final KeyCode code = event.getCode();
+			if (code == KeyCode.BACK_SPACE && searchString.length() > 0) {
+				searchString.setLength(searchString.length() - 1);
+			} else if (code == KeyCode.ESCAPE) {
+				searchString.setLength(0);
+			} else if (!code.isArrowKey() && !code.isFunctionKey() && !code.isMediaKey() && !code.isModifierKey()) {
+				searchString.append(event.getText());
+			} else
+				return;
 
-		final ObservableList<T> items = getItems();
-		for (int i = 0; i < items.size(); i++) {
-			if (items.get(i).toString().toLowerCase().startsWith(searchString.toString().toLowerCase())) {
-				setValue(items.get(i));
-				break;
+			final T item = findItem(searchString.toString());
+			if (item != null) {
+				setValue(item);
 			}
 		}
 	};
@@ -75,5 +90,13 @@ public class ReactiveComboBox<T> extends ComboBox<T> {
 
 		getEditor().textProperty().addListener(textListener);
 		setOnKeyPressed(searchHandler);
+	}
+
+	private final T findItem(final String prefix) {
+		final ObservableList<T> items = getItems();
+		for (int i = 0; i < items.size(); i++) {
+			if (items.get(i).toString().toLowerCase().startsWith(prefix.toLowerCase())) return items.get(i);
+		}
+		return null;
 	}
 }
