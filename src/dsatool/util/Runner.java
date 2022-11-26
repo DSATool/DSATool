3a83332560647328318bf951a68aab726245f64c
errt <dominik.helm@stud.tu-darmstadt.de>
2022-11-26 21:44:31 +0100

    Update auf JavaFX 19

--- src/dsatool/util/Runner.java
+++ src/dsatool/util/Runner.java
@@ -1,11 +1,15 @@
 package dsatool.util;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import dsatool.gui.Main;
 import javafx.application.Application;
 
 public class Runner {
 
 	public static void main(final String[] args) {
+		Logger.getLogger("javafx").setLevel(Level.SEVERE);
 		Application.launch(Main.class, args);
 	}
 }
