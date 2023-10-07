dc65016f75943ffd8aab317ad1861e6641ea78cf
errt <helm@cs.tu-darmstadt.de>
2023-10-07 15:48:41 +0200

    Setze Höhe für Tabellenkopf

--- src/dsatool/gui/GUIUtil.java
+++ src/dsatool/gui/GUIUtil.java
@@ -47,9 +47,14 @@ public class GUIUtil {
 	public static <T> void autosizeTable(final TableView<T> table) {
 		table.setColumnResizePolicy(GUIUtil::fixedWidthPolicy);
 
+		table.skinProperty().addListener((obs, ol, ne) -> {
+			final Pane header = (Pane) table.lookup("TableHeaderRow");
+			header.setPrefHeight(table.getFixedCellSize());
+		});
+
 		final DoubleBinding insetsHeight = Bindings.createDoubleBinding(() -> table.getInsets().getTop() + table.getInsets().getBottom(),
 				table.insetsProperty());
-		final DoubleBinding height = Bindings.size(table.getItems()).multiply(table.getFixedCellSize()).add(insetsHeight).add(26);
+		final DoubleBinding height = Bindings.size(table.getItems()).add(1).multiply(table.getFixedCellSize()).add(insetsHeight);
 		table.maxHeightProperty().bind(height);
 		table.minHeightProperty().bind(height);
 
